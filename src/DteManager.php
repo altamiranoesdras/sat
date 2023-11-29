1dc5085f0d876b36da1421d716f270995ea00d4f
Esdras Altamirano <altamiranoesdras@gmail.com>
2023-11-29 09:16:26 -0600

    Mejoras

--- src/DteManager.php
+++ src/DteManager.php
@@ -409,7 +409,7 @@ class DteManager extends VirtualAgency
                         'Emisor' => [
                             'DireccionEmisor' => [
                                 'Direccion' => ($current_branch->calleAvenida ?? '') . '  ' . ($current_branch->numeroCasa ?? '') .
-                                    ' ' . ($current_branch->colonia ?? ''). ', zona ' . $current_branch->zona . ', ' .
+                                    ' ' . ($current_branch->colonia ?? ''). ', zona ' . ($current_branch->zona ?? '0') . ', ' .
                                     $current_branch->municipio . ', ' . $current_branch->departamento,
                                 'CodigoPostal' => 1,
                                 'municipio' => $current_branch->municipio,
